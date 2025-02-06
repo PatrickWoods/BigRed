eed2f4663c8cd8448823d3fef1400d97a53097cc
PatrickWoods <patrick.woods@rogers.com>
2025-02-06 14:53:42 -0500

    Update hexmap.cpp

--- hexmap.cpp
+++ hexmap.cpp
@@ -6,21 +6,33 @@ namespace bigred
 {
   HexMap::HexMap()
   {
-    map_width = 0;
-    map_height = 0;
-    is_initialized = false;
+    m_map_width = 0;
+    m_map_height = 0;
+    m_rows = 0;
+    m_columns = 0;
+    m_is_initialized = false;
   }
 
   bool HexMap::Initialize(int width, int height)
   {
-    map_width = 0;
-    map_height = 0;
-    is_initialized = false;
+    m_map_width = 0;
+    m_map_height = 0;
+    m_rows = 0;
+    m_columns = 0;
+    m_is_initialized = false;
 
+    // this enforces a minimum map size, one that can be traversed
     if ((width >= 3) && (height >= 3))
     {
       map_width = width;
       map_height = height;
+
+      // odd columns + even columns
+      m_columns = (1 + ((m_map_width - 2)/3)) + (m_map_width / 3);
+
+      // odd rows + even rows
+      m_rows = ((int)((float)m_map_height / (2 * ALPHA))) + (1 + (int)((((float)m_map_height) - (3 * ALPHA))/(2 * ALPHA)));
+      
       is_initialized = true;
     }
 
